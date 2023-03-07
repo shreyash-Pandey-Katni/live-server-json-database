b1c200fbd7f2bc4ce598933d760648313055e16a
shreyashjiodndian2 <Shreyash.pandey.katni@gmail.com>
2023-03-07 21:18:36 +0530

    1st version

---
+++ README.md
@@ -0,0 +1,120 @@
+# Usage
+
+To run the application, execute the following command:
+
+```
+liveserver-json-database -f {file_path} -p {port}
+```
+
+Once the server is running, you can use the following endpoints to interact with the API:
+
+### POST /addData
+
+Adds a new data object to the file. The data object should be sent in the request body.
+
+Example request:
+
+```bash
+curl --request POST \
+  --url http://localhost:3000/addData \
+  --header 'Content-Type: application/json' \
+  --data '{
+ <your_data_object>
+}'
+```
+
+Example response:
+
+```bash
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+{
+  "success": true,
+  "msg": "Data added"
+}
+```
+
+### GET /listData
+
+Returns an array of all the data objects in the file.
+
+Example request:
+
+```bash
+curl --request GET \
+  --url http://localhost:3000/listData \
+  --header 'Content-Type: application/json'
+```
+
+Example response:
+
+```bash
+
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+[
+  {
+    "name": "John",
+    "age": 30
+  },
+  {
+    "name": "Jane",
+    "age": 25
+  }
+]
+```
+
+### GET /deleteData
+
+Deletes a data object from the file. The ID of the data object to delete should be sent in the request body.
+
+Example request:
+
+```bash
+curl --request GET \
+  --url http://localhost:3000/deleteData \
+  --header 'Content-Type: application/json' \
+  --data '{
+ "id":<id_of_data_object_to_delete>
+}'
+```
+
+Example response:
+
+```bash
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+{
+  "success": true,
+  "msg": "Data deleted"
+}
+```
+
+### GET /updateData
+
+Updates a data object in the file. The updated data object should be sent in the request body.
+
+Example request:
+
+```bash
+curl --request PUT \
+  --url 'http://localhost:3000/updateData/1465?=1465' \
+  --header 'Content-Type: application/json' \
+  --data '<dictionary of updated data>'
+```
+
+Example response:
+
+```bash
+
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+{
+  "success": true,
+  "msg": "Data updated"
+}
+```
---
+++ index.js
@@ -0,0 +1,97 @@
+#!/home/rootdaenib/.nvm/versions/node/v18.14.2/bin/node
+
+const yargs = require("yargs");
+
+
+const options = yargs
+    .usage("Usage: -f fileName")
+    .option("f", { alias: "fileName", describe: "File name", type: "string", demandOption: true })
+    .option("p", { alias: "port", describe: "Port", type: "number", demandOption: true })
+    .argv;
+
+const fs = require('fs');
+const path = require('path');
+const fileName = options.fileName;
+const filePath = path.join(__dirname, fileName);
+
+const express = require("express")
+const router = express.Router()
+const bodyParser = require("body-parser")
+// route
+
+const saveData = (data) => {
+    const stringData = JSON.stringify(data)
+    fs.writeFileSync(filePath, stringData)
+}
+
+const loadData = () => {
+    try {
+        const jsonData = fs.readFileSync(filePath)
+        return JSON.parse(jsonData)
+    } catch (e) {
+        console.log(e);
+        return []
+    }
+}
+
+router.post('/addData', (req, res) => {
+    try {
+        var existingAccounts = loadData()
+        const newAccountId = Math.floor(Math.random() * 1000000000 + 1)
+        existingAccounts[newAccountId] = req.body
+        saveData(existingAccounts)
+        res.send({ success: true, msg: "Account added" })
+    } catch (e) {
+        console.log(e);
+        res.send({ success: false, msg: "Account not added" })
+    }
+})
+
+router.get('/listData', (req, res) => {
+    try {
+        var existingAccounts = loadData()
+        res.send(existingAccounts)
+    }
+    catch (e) {
+        console.log(e);
+        res.send({ success: false, msg: "Account not found" })
+    }
+})
+
+router.get('/deleteData', (req, res) => {
+    try {
+        var existingAccounts = loadData()
+        delete existingAccounts[req.body.id]
+        saveData(existingAccounts)
+        res.send({ success: true, msg: "Account deleted" })
+    }
+    catch (e) {
+        console.log(e);
+        res.send({ success: false, msg: "Account not deleted" })
+    }
+})
+
+router.put('/updateData/:id', (req, res) => {
+    try {
+        var existingAccounts = loadData()
+        existingAccounts[req.params.id] = req.body
+        saveData(existingAccounts)
+        res.send({ success: true, msg: "Account updated" })
+    }
+    catch (e) {
+        console.log(e);
+        res.send({ success: false, msg: "Account not updated" })
+    }
+})
+
+const app = express()
+// middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+// route
+app.use('/', router)
+//start server
+
+app.listen(options.p, () => {
+    console.log(`listening at port:${options.p}`)
+})
